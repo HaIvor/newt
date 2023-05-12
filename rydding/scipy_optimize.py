aca5404954a0c1b63495acee3caf5ea913287ebe
HaIvor <45287121+HaIvor@users.noreply.github.com>
2023-05-12 11:05:48 +0200

    ny metode

--- rydding/scipy_optimize.py
+++ rydding/scipy_optimize.py
@@ -14,7 +14,7 @@ c1 = 1500 # Speed of sound, 1500 m/s
 t_d = r/c1 # Transmission delay t_d = r/c
 f_c = 24000 # 24 kHz
 p_lt = 0.001
-gamma = 31.62
+gamma = 32
 eta = 1
 
 log = np.log # The natural logarithm
@@ -24,7 +24,6 @@ log2 = np.log2 # Log base 2
 start_N = 512
 start_M = 4
 start_m = 5
-
 
 
 # x[0] = N, x[1] = M, x[2] = m 
@@ -38,21 +37,24 @@ cons = ({'type': 'ineq', 'fun': lambda x:  x[0] - 400},
         {'type': 'ineq', 'fun': lambda x: 64 - x[1]},
         {'type': 'ineq', 'fun': lambda x: x[2] - 1},
         {'type': 'ineq', 'fun': lambda x: 40 - x[2]},
-        {'type': 'ineq', 'fun': lambda x: -((x[2] * (x[0]/eta)*(log2(x[1])) * (0.2*np.exp(-((3*100)/(2* (x[1] - 1))))) ** (1 / R_c)) - 0.1)},
         {'type': 'ineq', 'fun': lambda x: -(log(x[1])+log(R_n) + log(x[0]) + log(log2(x[1])) + (1/R_c)*(log(0.2)-(3*gamma)/(2*(x[1]-1))) - log(p_lt))})
 
 
 # minimize f with initial guess x0 with constraints
-res = minimize(f, (start_N,start_M, start_m), constraints=cons) 
+res = minimize(f, (start_N,start_M, start_m), constraints=cons, method="SLSQP") 
+res2 = minimize(f, (start_N,start_M, start_m), constraints=cons, method="COBYLA")
 
-
-print("optimal verdi: \n")
+print("\noptimal value, method: SLSQP")
 print(f" N = {res.x[0]}, M = {res.x[1]}, m = {res.x[2]} \n\n with starting values = [{start_N}, {start_M}, {start_m}]")
 print(f"\n cost function is then equal to: {res.fun}")
 
-# Values coming from newton's method, ignore 
-# con123, 6
-print(f([1343.32638092,5.84360854,25.99181974]))
-# con123
-print(f([1382.96208666,28.34026602,23.12493068]))
-print("done")
+# # Values coming from newton's method, ignore 
+# # con123, 6
+# print(f([1343.32638092,5.84360854,25.99181974]))
+# # con123
+# print(f([1382.96208666,28.34026602,23.12493068]))
+# print("done")
+
+print("\n\n Optimal value, method: COBYLA")
+print(f" N = {res2.x[0]}, M = {res2.x[1]}, m = {res2.x[2]} \n\n with starting values = [{start_N}, {start_M}, {start_m}]")
+print("another method, function value: ", res2.fun)
